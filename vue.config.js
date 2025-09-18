c0b21bef6bc1a6cd6c8e5034eade91c86e60f2dd
Dmytrosat <dmytrosat@gmail.com>
2025-09-18 11:49:58 +0300

    Deploy Vue app to GitHub Pages: fix publicPath and include dist/

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,4 @@
 // vue.config.js
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/https://github.com/Dmytrosat/delivery-food-vue/'   // ⚠️ ВАЖЛИВО! Замініть на ім'я вашого репозиторію! (https://github.com/Dmytrosat/delivery-food-vue)
-    : '/'
+    publicPath: '/delivery-food-vue/' // ⚠️ ТОЧНЕ ІМ’Я РЕПОЗИТОРІЮ!
 }
