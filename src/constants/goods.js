624ff37797d4424b571c6324246081cfd84cf7fb
Dmytrosat <dmytrosat@gmail.com>
2025-09-18 11:12:26 +0300

    changes made to files, new files added

---
+++ src/constants/goods.js
@@ -0,0 +1,56 @@
+        // Array of cards goods
+    export const goodsArray = [{
+            id: 0,
+            title: 'Ролл угорь стандарт',
+            description: 'Рис, угорь, соус унаги, кунжут, водоросли нори.',
+            name: 'В корзину',
+            icons: 'basket-white.svg',
+            price: 250,
+            image: 'roll-standard.jpg'
+        },
+        {
+            id: 1,
+            title: 'Калифорния лосось стандарт',
+            description: 'ис, лосось, авокадо, огурец, майонез, икра масаго, водоросли нори.',
+            name: 'В корзину',
+            icons: 'basket-white.svg',
+            price: 250,
+            image: 'california-standard.jpg'
+        },
+        {
+            id: 2,
+            title: 'Окинава стандарт 185 г',
+            description: 'Рис, креветка отварная, сыр сливочный, лосось, огурец свежий...',
+            name: 'В корзину',
+            icons: 'basket-white.svg',
+            price: 250,
+            image: 'okinawa-standard.jpg'
+        },
+        {
+            id: 3,
+            title: 'Цезарь маки хl',
+            description: 'Рис, куриная грудка копченая, икра масаго, томат, айсберг, соус цезарь...',
+            name: 'В корзину',
+            icons: 'basket-white.svg',
+            price: 250,
+            image: 'caesar-poppies.jpg'
+        },
+        {
+            id: 4,
+            title: 'Ясай маки стандарт 185 г',
+            description: 'Рис, помидор свежий, перец болгарский, авокадо, огурец, айсберг',
+            name: 'В корзину',
+            icons: 'basket-white.svg',
+            price: 250,
+            image: 'poppies-standard.jpg'
+        },
+        {
+            id: 5,
+            title: 'Ролл с креветкой стандарт',
+            description: 'Рис, водоросли нори, креветки отварные, сыр сливочный, огурцы',
+            name: 'В корзину',
+            icons: 'basket-white.svg',
+            price: 250,
+            image: 'shrimp-standard.jpg'
+        }
+    ]
